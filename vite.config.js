81f5480e77b1f5d193c99afe493982a0d041c01f
nando-castro <fernando.castro10.master@gmail.com>
2025-01-30 09:12:34 -0300

    :sparkles: feat: adicionado configuração para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000", // Redireciona as chamadas de /api para o backend
+      "/api": "http://localhost:3000", // Mantém o proxy para chamadas de API
     },
+  },
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
 });
